4035c54cfca5ac2bafdc22848ea87df1cf39fffd
brandonblankenstein <brandon.blankenstein@gmail.com>
2022-05-21 02:21:51 -0500

    updated env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA-Njz-Uc1Ij_zeoNF18Pow2-zTF9BSBXs',
-  authDomain: 'gif-viewer-afe0e.firebaseapp.com',
-  projectId: 'gif-viewer-afe0e',
-  storageBucket: 'gif-viewer-afe0e.appspot.com',
-  messagingSenderId: '392531179914',
-  appId: '1:392531179914:web:1f96cf26719595e46b3073',
-  measurementId: 'G-HYV1JXPFFL'
+  apiKey: process.env.VUE_APP_GIPHY_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_GIPHY_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_GIPHY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_GIPHY_FIREBASE_STROAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_GIPHY_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_GIPHY_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_GIPHY_FIREBASE_MEASUREMENT_ID
 }
 
 const app = firebase.initializeApp(firebaseConfig)
